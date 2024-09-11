8af6e72fbb7a6d2021594b0e3a458d145fb2cc42
Amberly Ramsey <a.meghanramsey@gmail.com>
2024-09-11 18:56:52 -0400

    Updated Python File

--- adventure_log.py
+++ adventure_log.py
@@ -1,5 +1,5 @@
 def greet_coding_temple():
-    print("Ahoy, navigators! Ready for our next adventure?")
+    print("Ahoy, Coding Temple navigators!")
 
 if __name__ == "__main__":
     greet_coding_temple()
